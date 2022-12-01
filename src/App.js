3f406467a58aefc93cecc43467cd148579972272
Moamal-2000 <moamalalaapro1@gmail.com>
2022-12-01 19:48:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+
+function Div() {
+  return <div className="div1">Hello World I'm Coming</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Div />;
 }
 
 export default App;
